08377239f512c661bdb4b5eaa4a2f2680b706d24
Andres Soria <andres.zov@gmail.com>
2025-01-22 20:21:16 -0600

    Add dummy template content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './styles.css';
 
-console.log('Hello from index.js');
+const contentDiv = document.getElementById('content');
+contentDiv.innerHTML = `
+  <h1>Welcome to My Restaurant!</h1>
+  <p>We serve the best food in town. Visit us to enjoy our delicious menu.</p>
+  <img src="https://via.placeholder.com/400x300" alt="Restaurant Image">
+`;
